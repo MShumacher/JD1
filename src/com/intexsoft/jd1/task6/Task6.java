11652ba5d0408587cfa8f9fdd9d2cfb701167545
Олешкевич Павел <pashka-85@mail.ru>
2017-12-10 22:59:21 +0300

    no message

--- src/com/intexsoft/jd1/task6/Task6.java
+++ src/com/intexsoft/jd1/task6/Task6.java
@@ -2,10 +2,57 @@ package com.intexsoft.jd1.task6;
 
 public class Task6 {
 
+	static int d = 28;
+	static int m = 2;
+	static int y = 2016;
+
+	public static void isEndOfYear() {
+		if (m == 12) {
+			y++;
+			m = 1;
+		} else {
+			m++;
+		}
+	}
+
+	public static void IsEndOfMonth30() {
+		if (d == 31) {
+			d = 1;
+			isEndOfYear();
+		} else {
+			d++;
+		}
+	}
+
+	public static void IsEndOfMonth31() {
+		if (d == 30) {
+			d = 1;
+			m++;
+		} else {
+			d++;
+		}
+	}
+
+	public static void IsEndOfFebruary() {
+		if ((y % 4 == 0) && (d == 28)) {
+			d = 1;
+			m++;
+		} else {
+			d++;
+		}
+	}
+
+	public static void IsEndFebruaryAndLeapYear() {
+		if ((y % 4 != 0) && (d == 29)) {
+			d = 1;
+			m++;
+		} else {
+			d++;
+		}
+	}
+
 	public static void main(String[] args) {
-		int d = 28;
-		int m = 2;
-		int y = 2016;
+
 		switch (m) {
 		case 1:
 		case 3:
@@ -14,45 +61,19 @@ public class Task6 {
 		case 8:
 		case 10:
 		case 12:
-			if (d == 31) {
-				d = 1;
-				if (m == 12) {
-					y++;
-					m = 1;
-				} else {
-					m++;
-				}
-
-			} else {
-				d++;
-			}
+			IsEndOfMonth30();
 			break;
 		case 4:
 		case 6:
 		case 9:
 		case 11:
-			if (d == 30) {
-				d = 1;
-				m++;
-			} else {
-				d++;
-			}
+			IsEndOfMonth31();
 			break;
 		default:
-			if ((y % 4 == 0) && (d == 28)) {
-				d = 1;
-				m++;
-			} else {
-				d++;
-			}
-			if ((y % 4 != 0) && (d == 29)) {
-				d = 1;
-				m++;
-			} else {
-				d++;
-			}
+			IsEndOfFebruary();
+			IsEndFebruaryAndLeapYear();
 		}
-		System.out.println(String.format("Следующий день: %s.%s.%s", d, m, y));
+		System.out.println(String.format("Next date is: %s.%s.%s", d, m, y));
 	}
 
 }
