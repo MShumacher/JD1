0bc44b9a858be442bb704e49d1404ad8139242d9
Олешкевич Павел <pashka-85@mail.ru>
2017-12-12 23:25:05 +0300

    no message

--- src/com/itacademy/jd1/task25/BankCard.java
+++ src/com/itacademy/jd1/task25/BankCard.java
@@ -1,5 +1,15 @@
 package com.itacademy.jd1.task25;
 
+import java.util.Date;
+
 abstract class BankCard {
+	private String holderSurnameRu;
+	private String holderNameRu;
+	private String holderSurnameEn;
+	private String holderNameEn;
+	private String workPlace;
+	private int pin;
+	private int accountNumber;
+	private Date expirationDate;
 
 }
