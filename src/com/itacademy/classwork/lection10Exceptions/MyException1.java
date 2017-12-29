5ede59d39fd827529430906961fbb495efd547ba
Pavel Oleshkevich <pashka-85@mail.com>
2017-12-29 21:45:51 +0300

    no message

---
+++ src/com/itacademy/classwork/lection10Exceptions/MyException1.java
@@ -0,0 +1,10 @@
+package com.itacademy.classwork.lection10Exceptions;
+
+public class MyException1 extends Exception {
+
+	public MyException1() {
+		super();
+		System.out.println("exception created");
+	}
+
+}
---
+++ src/com/itacademy/classwork/lection10Exceptions/MyException2.java
@@ -0,0 +1,10 @@
+package com.itacademy.classwork.lection10Exceptions;
+
+public class MyException2 extends Exception {
+
+	public MyException2() {
+		super();
+		System.out.println("exception created");
+	}
+
+}
