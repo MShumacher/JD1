28da66cf88a86ee742b13e1e28fc30ea4f8767a3
Pavel Oleshkevich <pashka-85@mail.com>
2018-01-03 19:50:15 +0300

    no message

---
+++ src/com/itacademy/classwork/lection11/Task30.java
@@ -0,0 +1,27 @@
+package com.itacademy.classwork.lection11;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Task30 {
+
+	public static void main(String[] args) throws IOException {
+		String filePath = "d:\\oleole\\JD1\\src\\com\\itacademy\\classwork\\lection11\\task30.txt";
+		System.out.println(new File(filePath).exists());
+		List<String> lines = Files
+				.readAllLines(Paths.get(filePath), Charset.forName("cp1251"));
+	
+		int wordsCount = 0;
+		for (String string : lines) {
+			System.out.println(string);
+			String[] split = string.split(" ");
+			wordsCount += split.length;
+		}
+		System.out.println(String.format("Text in file has %s words.", wordsCount));
+	}
+
+}
