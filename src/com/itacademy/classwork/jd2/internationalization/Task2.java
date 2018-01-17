d1577e1b774ae8320ff068e63888cbb3e4704c52
Pavel Oleshkevich <pashka-85@mail.com>
2018-01-17 20:26:50 +0300

    no message

---
+++ src/com/itacademy/classwork/jd2/internationalization/Task2.java
@@ -0,0 +1,15 @@
+package com.itacademy.classwork.jd2.internationalization;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
+public class Task2 {
+
+	public static void main(String[] args) throws ParseException {
+		Locale locale = new Locale("ru", "RU");
+		NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
+		System.out.println(numberFormat.format(15300000.5));
+	}
+
+}
---
+++ src/com/itacademy/classwork/jd2/internationalization/Task7.java
@@ -0,0 +1,14 @@
+package com.itacademy.classwork.jd2.internationalization;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Task7 {
+
+	public static void main(String[] args) {
+		Locale locale = new Locale("en", "EN");
+		ResourceBundle rb = ResourceBundle.getBundle("MessagesBundle", locale);
+		System.out.println(rb.getString("greetings"));
+	}
+
+}
